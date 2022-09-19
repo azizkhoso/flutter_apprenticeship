71580404dab076e048e362d1f871f1230d867eb7
azizkhoso <azizkhoso586@gmail.com>
2022-09-19 06:31:58 +0500

    Added Custom theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_apprenticeship/fooderlich_theme.dart';
 // import 'fooderlich_theme.dart';
 
 void main() {
-  runApp(const Fooderlich());
+  runApp(Fooderlich());
 }
 
 class Fooderlich extends StatelessWidget {
-  const Fooderlich({Key? key}) : super(key: key);
+  Fooderlich({Key? key}) : super(key: key);
+  final theme = ThemeData(textTheme: FooderlichTheme.lightTextTheme);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: theme,
       title: 'Fooderlich',
       home: Scaffold(
         appBar: AppBar(
@@ -17,9 +20,10 @@ class Fooderlich extends StatelessWidget {
             'Fooderlich',
           ),
         ),
-        body: const Center(
+        body: Center(
           child: Text(
             'Let\'s get cooking',
+            style: theme.textTheme.headline6,
           ),
         ),
       ),
